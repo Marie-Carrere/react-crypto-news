8af0b7f215ddfa3e81c735121a619c78396c8000
Marie Carrere <marie.carrere@icloud.com>
2018-10-22 10:42:09 +0200

    detail route ok

--- src/components/list/Table.js
+++ src/components/list/Table.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './Table.css';
 import PropTypes from 'prop-types';
 
 const Table = (props) => {
-    const { currencies, renderChangePercent } = props;
+    const { currencies, renderChangePercent, history } = props;
 
     return(
             <div className="Table-container">
@@ -19,7 +20,10 @@ const Table = (props) => {
                 </thead>
                 <tbody className="Table-body">
                     {currencies.map((currency) => (
-                        <tr key={currency.id}>
+                        <tr
+                         key={currency.id}
+                         onClick={() => history.push(`/currency/${currency.id}`)}
+                         >
                             <td>
                                 <span className="Table-rank">{currency.rank}</span>
                                 {currency.name}
@@ -43,9 +47,11 @@ const Table = (props) => {
             );
 }
 
+// Props validation
 Table.propTypes = {
     currencies: PropTypes.array.isRequired,
     renderChangePercent: PropTypes.func.isRequired,
+    history: PropTypes.object.isRequired,
 };
 
-export default Table;
+export default withRouter(Table);
